d008a53fb24b9a2b6e6f2aaa6afb928d1e02a517
MrGorillaz <Alexander_Orlowski@gmx.de>
2023-12-21 00:03:06 +0100

    day10

---
+++ day11/day11_2._incomplete.py
@@ -0,0 +1,144 @@
+import numpy as np
+from tqdm import tqdm
+import itertools
+file = open("day11_input1.txt","r")
+
+values = file.readlines()
+file.close()
+
+for line in range(len(values)):
+    values[line] = values[line].strip()
+    values[line] = list(values[line])
+
+
+
+
+
+def expand_universe(universe):
+
+    expand_line_pos = []
+    expand_row_pos = []
+
+    universe = np.array(universe)
+
+    for line in range(len(universe)):
+        if ("#" in universe[line]):
+            pass
+        else:
+            expand_line_pos.append(line)
+    
+    #tranpose matrix of universe
+    trans_universe = universe.transpose()
+
+
+    for line in range(len(trans_universe)):
+
+        if ("#") in trans_universe[line]:
+            pass
+        else:
+            expand_row_pos.append(line)
+
+    print("expand-Rows")
+    for expand_row in expand_row_pos:
+        #for i in tqdm(enumerate(range(1_000_000))):
+            #universe = np.insert(universe,expand_row,".",axis=1)
+        universe = [np.insert(universe,expand_row,".",axis=1) for i in tqdm(range(999_999))]
+    
+    print("expand-Lines")
+    for expand_line in expand_line_pos:
+        #for i in tqdm(range(1_000_000)):
+         #universe = np.insert(universe,expand_line,".",axis=0)
+        universe = [np.insert(universe,expand_line,".",axis=0) for i in tqdm(range(999_999))]
+
+    
+    
+    galaxy = 0
+
+    #for line in universe:
+    #    for i in range(len(line)):
+    #        if (line[i] == "#"):
+    #            galaxy = galaxy+1
+    #            line[i] = galaxy
+    
+
+    return universe
+
+
+def get_galaxy_positions(universe):
+
+    galaxy_count = (universe != ".").sum()
+    galaxies = np.where(universe == "#")
+    galaxy_pos = []
+
+    for galaxy in range(len(galaxies[0])):
+
+        galaxy_pos.append([galaxies[0][galaxy],galaxies[1][galaxy]])
+        
+        #if ([galaxies[1][galaxy],galaxies[0][galaxy]] in galaxy_pos):
+        #    galaxy_pos.remove([galaxies[1][galaxy],galaxies[0][galaxy]])
+        
+
+    return galaxy_pos
+
+
+def get_galaxy_positions_distances(galaxy_pos):
+
+    distance_sum = 0
+    galaxy_pairs = []
+
+    
+
+    for pos in galaxy_pos:
+
+
+        for i in range(len(galaxy_pos)):
+
+            #for g in galaxy_pairs:
+
+            #   if [g[1],g[0]] in galaxy_pairs:
+            #        galaxy_pairs.remove([g[1],g[0]])
+
+            galaxy_pair = [pos,galaxy_pos[i]]
+           #galaxy_pair.sort(key=lambda x: x[0])
+            if galaxy_pair not in galaxy_pairs:
+                if ((galaxy_pair[0][0] != galaxy_pair[1][0]) and (galaxy_pair[0][1] != galaxy_pair[1][1])):
+                    galaxy_pairs.append(galaxy_pair)
+                elif((galaxy_pair[0][0] != galaxy_pair[1][0]) and (galaxy_pair[0][1] == galaxy_pair[1][1])):
+                    galaxy_pairs.append(galaxy_pair)
+                elif((galaxy_pair[0][0] == galaxy_pair[1][0]) and (galaxy_pair[0][1] != galaxy_pair[1][1])):
+                    galaxy_pairs.append(galaxy_pair)
+                else:
+                    print(galaxy_pair)
+    
+    print("remove double pairs")
+    for g in galaxy_pairs:
+            print("remove double pairs")
+            if [g[1],g[0]] in galaxy_pairs:
+                galaxy_pairs.remove([g[1],g[0]])
+
+   
+    print("calculate")
+    for pos in galaxy_pairs:
+        
+        distance = abs(pos[0][0] - pos[1][0]) + abs(pos[0][1]-pos[1][1])
+        #if distance == 5:
+        #    print(pos)
+        distance_sum = distance_sum + distance
+
+    return distance_sum
+
+
+
+
+
+universe = expand_universe(values)
+
+galaxy_pos = get_galaxy_positions(universe)
+
+all_galaxy_distances = get_galaxy_positions_distances(galaxy_pos)
+
+print (all_galaxy_distances)
+
+
+
+
